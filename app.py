1c89c4383d346b53597f265d5ed83236949d3a55
rahulsound <32321373+rahulsound@users.noreply.github.com>
2024-08-20 11:24:06 +0530

    profile-report-try

--- app.py
+++ app.py
@@ -1,4 +1,23 @@
 import streamlit as st
 import pandas as pd
+import os
+from ydata_profiling import ProfileReport
+from streamlit_pandas_profiling import st_profile_report
+
 
 st.header('EDA App:')
+run = 'rome_stati_medium'
+sched = 'sched0'
+tr = 'tr0'
+exp = 'exp1'
+bs = 'bs1'
+
+file = run + os.path.sep + sched + os.path.sep + tr + os.path.sep + exp + os.path.sep + bs + '.csv'
+st.write(file)
+df = pd.read_csv(file)
+
+st.write(df.head())
+
+#pr = ProfileReport(df, title="Profiling Report")
+#st_profile_report(pr)
+
