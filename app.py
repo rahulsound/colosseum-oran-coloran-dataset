59f621146079e7cf7fcf652329932a7711558016
rahulsound <32321373+rahulsound@users.noreply.github.com>
2024-08-20 11:29:05 +0530

    .

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from streamlit_pandas_profiling import st_profile_report
 st.header('EDA App:')
 run = 'rome_static_medium'
 sched = 'sched0'
-tr = 'tr22'
+tr = 'tr0'
 exp = 'exp1'
 bs = 'bs1'
 csv = 'bs1'
